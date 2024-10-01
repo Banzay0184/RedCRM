5e126e6c37f66dd5b2a7ee00bf6742d4228af17d
Banzay0184 <shakhzodabidov@gmail.com>
2024-10-01 18:00:35 +0500

    0.09

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/login/', {
+      const response = await axios.post('https://sabina0184.pythonanywhere.com/api/login/', {
         username: email,
         password: password,
       });

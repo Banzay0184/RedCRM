f7adcb7643c07ebe6e7dc1eaf1ce21dd63014d5d
Banzay0184 <shakhzodabidov@gmail.com>
2024-10-30 19:48:59 +0500

    0.04

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -91,13 +91,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "red_crm",
-        "USER": "shakhzodabidov",
-        "PASSWORD": "admin1234",
-        "HOST": "localhost",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

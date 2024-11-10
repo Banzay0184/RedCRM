eab6575fea2b8f4347ec870add6d97cc2a11b5b5
Banzay0184 <shakhzodabidov@gmail.com>
2024-11-10 23:54:04 +0500

    0.8

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,11 +25,11 @@ SECRET_KEY = "django-insecure-2^39efq6=s5rt2#-5%ad8@i0&5+!jk3k@7pdizh1fan)oyax5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'mukhammadrizo07.pythonanywhere.com'
-]
+ALLOWED_HOSTS = ['mukhammadrizo07.pythonanywhere.com']
+# ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
+    # 'http://localhost:5173'
     "https://red-crm-beta.vercel.app",
 ]
 

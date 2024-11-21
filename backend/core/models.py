2ba6d42108f1caa22c0b46ef8ea7f381f9f2ea7c
Banzay0184 <shakhzodabidov@gmail.com>
2024-11-22 00:24:18 +0500

    2.2

--- backend/core/models.py
+++ backend/core/models.py
@@ -56,7 +56,7 @@ class Workers(PhoneNumber, BaseModel):
     """Модель сотрудников."""
 
     name = models.CharField(max_length=255)
-    order = models.PositiveIntegerField(default=0)
+    order = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.name

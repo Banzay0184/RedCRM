ad69d0edc7553b7969c88ab1ef25ab895a33288e
Banzay0184 <shakhzodabidov@gmail.com>
2025-12-17 18:03:41 +0500

    deploy 1.0

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,10 +42,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 # Безопасность: ALLOWED_HOSTS из переменных окружения
 ALLOWED_HOSTS = [
-    '178.18.243.236:8000',
     '178.18.243.236',
-    'api.redcrm.uz'
+    'api.redcrm.uz',
 ]
+
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',

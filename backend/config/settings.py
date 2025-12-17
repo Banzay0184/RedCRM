ddee7b2b940689fb8ee6d9be864ea68bbd7021c8
Banzay0184 <shakhzodabidov@gmail.com>
2025-12-17 17:01:57 +0500

    deploy 0.4

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,7 +18,10 @@ import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production

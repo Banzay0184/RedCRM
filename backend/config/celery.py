e6267da27dcead74e20e8432219582b2e9f53c86
Banzay0184 <shakhzodabidov@gmail.com>
2025-12-20 16:27:56 +0500

    deploy 1.4

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,8 +1,7 @@
-# project/celery.py
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from celery.schedules import crontab
+from celery.schedules import crontab  # импортируем crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -11,10 +10,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 # Настройка расписания для автоматической отправки уведомлений работникам
-# Задача будет запускаться каждые 5 минут, чтобы сверять время отправки
 app.conf.beat_schedule = {
     'check-and-send-worker-notifications': {
         'task': 'core.tasks.send_worker_event_notifications',
-        'schedule': crontab(hour=12, minute=30),
+        'schedule': crontab(minute=35, hour=16),  # каждый день в 21:30
     },
 }
